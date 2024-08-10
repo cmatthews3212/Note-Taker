32764c906d78d3c39681dc65a7e80066983b5c32
Christine Matthews <candreola@gmail.com>
2024-08-10 15:48:04 -0500

    Post request receives note from front end

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,6 +1,8 @@
 const notes = require('express').Router();
 const fs = require('fs');
 const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+const { readAndAppend, readFromFile } = require('../helpers/fsUtils.js');
 
 notes.get('/', (req, res) => {
     fs.readFile(path.join('../develop/db/db.json'), 'utf8', (err, data) => {
@@ -13,11 +15,29 @@ notes.get('/', (req, res) => {
     
 })
 
-// notes.get('/notes', (req, res) => {
-//     fs.readFile('./db/db.json', req.body, (err) => {
-//       console.error(err);
-//     })
-//     console.log(req.body);
-// });
+notes.post('/', (req, res) => {
+    console.info(`${req.method} request received about note submission`);
+
+    const { title, text } = req.body;
+
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            note_id: uuidv4(),
+        }
+
+        readAndAppend(newNote, '../develop/db/db.json');
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
+
+        res.json(response);
+
+    } else {
+        res.json('Error in posting note')
+    }
+})
 
 module.exports = notes;
